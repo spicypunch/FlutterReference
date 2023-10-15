95b73d213d1bac115882902eaf7eb000a6dd700d
spicypunch <miniguni66@naver.com>
2023-10-15 23:37:41 +0900

    backup

--- ADV_BASICS/lib/main.dart
+++ ADV_BASICS/lib/main.dart
@@ -1,17 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.deepPurple, Colors.black87],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          child: const StartScreen()),
-    ),
-  ));
+  runApp(const Quiz());
 }
--- ADV_BASICS/lib/start_screen.dart
+++ ADV_BASICS/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -10,6 +12,7 @@ class StartScreen extends StatelessWidget {
         Image.asset(
           'assets/images/quiz-logo.png',
           width: 300,
+          color: const Color.fromARGB(150, 255, 255, 255),
         ),
         const SizedBox(height: 80),
         const Text(
@@ -19,8 +22,11 @@ class StartScreen extends StatelessWidget {
         const SizedBox(
           height: 30,
         ),
-        OutlinedButton(
-            onPressed: () {}, style: OutlinedButton.styleFrom(foregroundColor: Colors.white), child: const Text('Start Quiz'))
+        OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'))
       ]),
     );
   }
