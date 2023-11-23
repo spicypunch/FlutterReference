ec1aa91c933e9d5d585ea395fad6aad46097e0c2
spicypunch <miniguni66@naver.com>
2023-11-23 22:50:33 +0900

    FlutterReference

--- u_and_i/lib/main.dart
+++ u_and_i/lib/main.dart
@@ -4,7 +4,18 @@ import 'package:u_and_i/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+              color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0),
+          displaySmall: TextStyle(
+              color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.w700),
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 30.0),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+      ),
       home: HomeScreen(),
-    )
+    ),
   );
 }
