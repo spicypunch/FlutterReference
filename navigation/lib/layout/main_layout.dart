e0009b445dbf3117d4fb9cafffeff1cec89cbc9a
spicypunch <miniguni66@naver.com>
2023-12-21 22:00:52 +0900

    백업

---
+++ navigation/lib/layout/main_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  const MainLayout({required this.title, required this.children, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$title'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
---
+++ navigation/lib/screen/route_one_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_two_screen.dart';
+
+class RouteOneScreen extends StatelessWidget {
+  final int? num;
+
+  const RouteOneScreen({this.num, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: '11111',
+      children: [
+        Text(
+          'arguments: ${num.toString()}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print(Navigator.of(context).canPop());
+          },
+          child: Text('Can Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).maybePop();
+          },
+          child: Text('Maybe Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop(456);
+          },
+          child: Text('Pop'),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (_) => RouteTwoScreen(),
+                    settings: RouteSettings(arguments: 789)),
+              );
+            },
+            child: Text("Push"))
+      ],
+    );
+  }
+}
