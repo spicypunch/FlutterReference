b4a27f7cd97792f0a40f01f5283a43dbca788b2e
spicypunch <miniguni66@naver.com>
2024-01-02 22:42:11 +0900

    reference

--- player/lib/component/custom_video_player.dart
+++ player/lib/component/custom_video_player.dart
@@ -6,8 +6,10 @@ import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
+  final VoidCallback onNewVideoPressed;
 
-  const CustomVideoPlayer({required this.video, super.key});
+  const CustomVideoPlayer(
+      {required this.video, required this.onNewVideoPressed, super.key});
 
   @override
   State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
@@ -15,21 +17,40 @@ class CustomVideoPlayer extends StatefulWidget {
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoController;
+  Duration currentPosition = Duration();
+  bool showControls = false;
 
   @override
   void initState() {
     super.initState();
 
-    initializeController()
+    initializeController();
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomVideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // oldWidget은 새로운 위젯이 생성되기 전의 위젯
+    if (oldWidget.video.path != widget.video.path) {
+      initializeController();
+    }
   }
 
   initializeController() async {
+    currentPosition = Duration();
+
     videoController = VideoPlayerController.file(File(widget.video.path));
     await videoController?.initialize();
-    
-    setState(() {
-      
+
+    // 슬라이더 영상 재생에 맞게 이동
+    videoController!.addListener(() {
+      final currentPosition = videoController!.value.position;
+      setState(() {
+        this.currentPosition = currentPosition;
+      });
     });
+
+    setState(() {});
   }
 
   @override
@@ -37,6 +58,193 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     if (videoController != null) {
       return CircularProgressIndicator();
     }
-    return VideoPlayer(videoController!);
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            showControls = !showControls;
+          });
+        },
+        child: Stack(
+          children: [
+            VideoPlayer(videoController!),
+            if (showControls)
+              _Controls(
+                onReversePressed: onReversePressed,
+                onPlayPressed: onPlayPressed,
+                onForwardPressed: onForwardPressed,
+                isPlaying: videoController!.value.isPlaying,
+              ),
+            if (showControls)
+              _NewVideo(
+                onPressed: widget.onNewVideoPressed,
+              ),
+            _SliderBottom(
+              currentPosition: currentPosition,
+              maxPosition: videoController!.value.duration,
+              onSliderChanged: onSliderChanged,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    videoController!.seekTo(Duration(
+      seconds: val.toInt(),
+    ));
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+
+    Duration position = Duration();
+
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - const Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onForwardPressed() {
+    final maxPosition = videoController!.value.duration;
+    final currentPosition = videoController!.value.position;
+
+    Duration position = maxPosition;
+
+    if ((maxPosition - Duration(seconds: 3)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition - const Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    // 이미 실행 중이면 중지
+    // 실행 중이 아니면 실행
+    setState(() {
+      if (videoController!.value.isPlaying) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controls(
+      {required this.onPlayPressed,
+      required this.onReversePressed,
+      required this.onForwardPressed,
+      required this.isPlaying,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      height: MediaQuery.of(context).size.height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+              onPressed: onReversePressed, iconData: Icons.rotate_left),
+          renderIconButton(
+              onPressed: onPlayPressed,
+              iconData: isPlaying ? Icons.pause : Icons.play_arrow),
+          renderIconButton(
+              onPressed: onForwardPressed, iconData: Icons.rotate_right),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton(
+      {required VoidCallback onPressed, required IconData iconData}) {
+    return IconButton(
+        onPressed: onPressed,
+        iconSize: 30.0,
+        color: Colors.white,
+        icon: Icon(iconData));
+  }
+}
+
+class _NewVideo extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _NewVideo({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      // 오른쪽 끝에서 부터 0 픽셀 움직이라는 뜻
+      right: 0,
+      child: IconButton(
+        onPressed: onPressed,
+        color: Colors.white,
+        iconSize: 30.0,
+        icon: Icon(Icons.photo_camera_back),
+      ),
+    );
+  }
+}
+
+class _SliderBottom extends StatelessWidget {
+  final Duration currentPosition;
+  final Duration maxPosition;
+  final ValueChanged<double> onSliderChanged;
+
+  const _SliderBottom(
+      {required this.currentPosition,
+      required this.maxPosition,
+      required this.onSliderChanged,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Text(
+              // padLeft는 왼쪽 공간에 빈공간 대신 글자를 넣는 함수
+              '${currentPosition.inMinutes}:${(currentPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Slider(
+                value: currentPosition.inSeconds.toDouble(),
+                onChanged: onSliderChanged,
+                max: maxPosition.inSeconds.toDouble(),
+                min: 0,
+              ),
+            ),
+            Text(
+              // padLeft는 왼쪽 공간에 빈공간 대신 글자를 넣는 함수
+              '${maxPosition.inMinutes}:${(maxPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
