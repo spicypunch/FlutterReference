06fa7ba83c036296defdef44674d190e1b841aa8
spicypunch <miniguni66@naver.com>
2024-02-05 22:21:39 +0900

    백업

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,3 @@
+const APP_ID = 'bd29d744a39c4598ba39f6e7b0126bd9';
+const CHANNEL_NAME = 'videoCallChannel';
+const TEMP_TOKEN = '007eJxTYOCpy152wHXrw5zPizdV2wVs3mbNln+rckKGrK/zBf2Pfx4qMCSlGFmmmJuYJBpbJpuYWlokARlpZqnmSQaGRmZJKZYMlw+kNgQyMmhNDGJhZIBAEF+AoSwzJTXfOTEnxzkjMS8vNYeBAQBLqCTu';
