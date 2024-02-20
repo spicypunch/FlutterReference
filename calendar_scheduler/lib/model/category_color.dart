a800df73e4c9bc83f9a5c69b18be93ebf6603d37
김종민 <jm.kim@lifesemantics.kr>
2024-02-20 10:09:21 +0900

    백업

--- calendar_scheduler/lib/model/category_color.dart
+++ calendar_scheduler/lib/model/category_color.dart
@@ -1,7 +1,7 @@
 import 'package:drift/drift.dart';
 
 class CategoryColors extends Table {
-  IntColumn get id => integer()();
+  IntColumn get id => integer().autoIncrement()();
 
   TextColumn get hexCode => text()();
 
