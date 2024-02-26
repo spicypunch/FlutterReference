4611633804bb83ac95c07fe4dc004def97e3e4e1
김종민 <jm.kim@lifesemantics.kr>
2024-02-26 14:11:00 +0900

    백업

---
+++ scrollable_widgets/android/app/src/main/kotlin/kr/jm/scrollable_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.jm.scrollable_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ scrollable_widgets/lib/layout/main_layout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  final String title;
+  final Widget body;
+
+  const MainLayout({required this.title, required this.body, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: body,
+    );
+  }
+}
