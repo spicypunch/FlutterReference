eaf25d1b65e31bc123c4b88e246f3f58a1019eae
김종민 <jm.kim@lifesemantics.kr>
2024-03-06 09:31:15 +0900

    백업

--- scrollable_widgets/lib/screen/home_screen.dart
+++ scrollable_widgets/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
+import 'package:scrollable_widgets/screen/grid_view_screen.dart';
 import 'package:scrollable_widgets/screen/list_view_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll_view.dart';
 
@@ -19,6 +20,7 @@ class HomeScreen extends StatelessWidget {
         builder: (_) => SingleChildScrollViewScreen(),
         name: 'SingleChildScrollViewScreen'),
     ScreenModel(builder: (_) => ListViewScreen(), name: 'ListViewScreen'),
+    ScreenModel(builder: (_) => GridViewScreen(), name: 'GridViewScreen'),
   ];
 
   HomeScreen({super.key});
--- scrollable_widgets/lib/screen/list_view_screen.dart
+++ scrollable_widgets/lib/screen/list_view_screen.dart
@@ -11,7 +11,7 @@ class ListViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MainLayout(
       title: 'ListViewScreen',
-      body: renderBuilder(),
+      body: renderDefault(),
     );
   }
 
@@ -31,22 +31,41 @@ class ListViewScreen extends StatelessWidget {
   // 필요한 부분만 그림
   Widget renderBuilder() {
     return ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) {
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index);
+      },
+    );
+  }
+
+  // 필요한 부분만 그리고 중간에 필요한 위젯을 넣을 수 있음
+  Widget renderSeparated() {
+    return ListView.separated(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index);
+      },
+      separatorBuilder: (context, index) {
+        // 5개의 item마다 배너 보여주기
+        if (index % 5 == 0) {
           return renderContainer(
-              color: rainbowColors[index % rainbowColors.length],
-              index: index);
-        });
+              color: Colors.black, index: index, height: 100);
+        }
+        return Container();
+      },
+    );
   }
 
   Widget renderContainer({
     required Color color,
     required int? index,
+    double? height,
   }) {
     print(index);
-
     return Container(
-      height: 300,
+      height: height ?? 300,
       color: color,
       child: Center(
         child: Text(
