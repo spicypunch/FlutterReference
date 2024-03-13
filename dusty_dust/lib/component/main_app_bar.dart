de14f1bd78069cd1741e90800ee078fce2128edf
spicypunch <miniguni66@naver.com>
2024-03-13 22:33:53 +0900

    백업

--- dusty_dust/lib/component/main_app_bar.dart
+++ dusty_dust/lib/component/main_app_bar.dart
@@ -30,20 +30,28 @@ class MainAppBar extends StatelessWidget {
                     fontSize: 20.0,
                   ),
                 ),
-                const SizedBox(height: 20.0,),
+                const SizedBox(
+                  height: 20.0,
+                ),
                 Image.asset(
                   'asset/img/mediocre.png',
                   width: MediaQuery.of(context).size.width / 2,
                 ),
-                const SizedBox(height: 20.0,),
+                const SizedBox(
+                  height: 20.0,
+                ),
                 Text(
                   '보통',
-                  style: ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
+                  style:
+                      ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
                 ),
-                const SizedBox(height: 20.0,),
+                const SizedBox(
+                  height: 20.0,
+                ),
                 Text(
                   '나쁘지 않네요!',
-                  style: ts.copyWith(fontSize: 20.0, fontWeight: FontWeight.w700),
+                  style:
+                      ts.copyWith(fontSize: 20.0, fontWeight: FontWeight.w700),
                 ),
               ],
             ),
