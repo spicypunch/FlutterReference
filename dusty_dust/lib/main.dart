ae831e6b99100c4bc7ade0cecc1936f49221834f
김종민 <jm.kim@lifesemantics.kr>
2024-03-13 09:45:09 +0900

    백업

--- dusty_dust/lib/main.dart
+++ dusty_dust/lib/main.dart
@@ -2,5 +2,12 @@ import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen(),));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+      ),
+      home: HomeScreen(),
+    ),
+  );
 }
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,13 +1,20 @@
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello'),
+      backgroundColor: primaryColor,
+      drawer: MainDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar(),
+        ],
       ),
     );
   }
