5126bab7873ac01d7f42e4359cec4c1a79f69255
spicypunch <miniguni66@naver.com>
2024-08-02 12:59:58 +0900

    backup

---
+++ basic_ui/android/app/src/main/kotlin/kr/jm/basic_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.jm.basic_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ basic_ui/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:basic_ui/common/component/custom_text_form_field.dart';
+import 'package:basic_ui/user/view/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
