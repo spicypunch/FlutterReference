056460744b0a5a68eada883724e2297368d67149
spicypunch <miniguni66@naver.com>
2024-08-21 22:12:26 +0900

    backup

---
+++ riverpod_practice/lib/screen/family_modifier_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_practice/layout/default_layout.dart';
+import 'package:riverpod_practice/riverpod/family_modifier_provider.dart';
+
+class FamilyModifierScreen extends ConsumerWidget {
+  const FamilyModifierScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(familyModifierProvider(10));
+    return DefaultLayout(
+      title: 'FamilyModifierScreen',
+      body: Center(
+          child: state.when(
+        data: (data) => Text(
+          data.toString(),
+        ),
+        error: (err, stack) => Text(
+          err.toString(),
+        ),
+        loading: () => CircularProgressIndicator(),
+      )),
+    );
+  }
+}
--- riverpod_practice/lib/screen/home_screen.dart
+++ riverpod_practice/lib/screen/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_practice/layout/default_layout.dart';
+import 'package:riverpod_practice/screen/auto_dispose_modifier_screen.dart';
+import 'package:riverpod_practice/screen/family_modifier_screen.dart';
 import 'package:riverpod_practice/screen/future_provider_screen.dart';
+import 'package:riverpod_practice/screen/listen_provider_screen.dart';
+import 'package:riverpod_practice/screen/provider_screen.dart';
+import 'package:riverpod_practice/screen/select_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_provider_screen.dart';
 import 'package:riverpod_practice/screen/stream_provider_screen.dart';
@@ -53,6 +58,56 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SelectProviderScreen(),
+                ),
+              );
+            },
+            child: Text('SelectProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ProviderScreen'),
           ),
         ],
       ),
